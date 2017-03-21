3269aca3ca8beb8a566d7b37ff39d5dfeb420547
Hawken Rives <hawkrives@gmail.com>
2017-03-20 22:24:48 -0500

    move as much stuff to the top-level as possible

--- env.sh
+++ env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-BMC_ROOT="/Users/hawken/Dropbox/Benchmark Photos"
-BMC_DATA="/Users/hawken/Dropbox/Benchmark Photos/_app"
+# BMC_ROOT="/Users/hawken/Dropbox/Benchmark Photos"
+# BMC_DATA="/Users/hawken/Dropbox/Benchmark Photos/_app"
+
+BMC_ROOT="/Users/hawken/Sites/bmc-photos-test"
+BMC_DATA="/Users/hawken/Sites/bmc-photos-app-data"
 
 export BMC_PHOTOS_DIR="$BMC_ROOT/photos"
 export BMC_PHOTOS_UNSORTED_DIR="$BMC_ROOT/unsorted"
 
 export BMC_METADATA_DIR="$BMC_DATA/metadata"
+export BMC_THUMBNAILS_DIR="$BMC_DATA/thumbnails"
 export BMC_DATABASE_FILE="$BMC_DATA/database.json"
