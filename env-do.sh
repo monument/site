0b066f66a56638c3e5473e8a6e0f2138447e7703
Hawken Rives <hawkrives@gmail.com>
2017-03-23 12:19:27 -0500

    add env for digitalocean

---
+++ env-do.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BMC_ROOT="/mnt/volume-nyc1-02/Dropbox/Benchmark Photos"
+BMC_DATA="/mnt/volume-nyc1-02/Dropbox/Benchmark Photos/_data"
+
+export BMC_PHOTOS_DIR="$BMC_ROOT/photos"
+export BMC_PHOTOS_UNSORTED_DIR="$BMC_ROOT/to sort"
+
+export BMC_METADATA_DIR="$BMC_DATA/metadata"
+export BMC_THUMBNAILS_DIR="$BMC_DATA/thumbnails"
+export BMC_DATABASE_FILE="$BMC_DATA/database.json"
--- env.sh
+++ env.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# BMC_ROOT="/Users/hawken/Dropbox/Benchmark Photos"
-# BMC_DATA="/Users/hawken/Dropbox/Benchmark Photos/_app"
-
-BMC_ROOT="/Users/hawken/Sites/bmc-photos-test"
-BMC_DATA="/Users/hawken/Sites/bmc-photos-app-data"
-
-export BMC_PHOTOS_DIR="$BMC_ROOT/photos"
-export BMC_PHOTOS_UNSORTED_DIR="$BMC_ROOT/unsorted"
-
-export BMC_METADATA_DIR="$BMC_DATA/metadata"
-export BMC_THUMBNAILS_DIR="$BMC_DATA/thumbnails"
-export BMC_DATABASE_FILE="$BMC_DATA/database.json"
+if [[ $DIGITAL_OCEAN ]]; then
+    source ../env-do.sh
+else
+    source ../env-local.sh
+fi
